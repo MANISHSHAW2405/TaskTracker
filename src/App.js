4464b3784c87a778954892809bfa2db2b03ce0ce
MANISH SHAW <manish.kolkata99@gmail.com>
2021-05-03 17:23:32 +0530

    deploy 2nd try

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from './components/Header'
+import {useState} from 'react'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ const [showAddTask,setShowAddTask]=useState(false)
+  const[tasks,setTasks]=useState([
+    {
+           id : 1,
+           text : 'doc appointment',
+           day: 'feb 5th at 2:30',
+           reminder : true,
+   },{
+       id:2,
+       text : 'meetng at school',
+       day : 'feb 6th at 1:30pm',
+       reminder : true,
+   },{
+       id:3,
+       text : 'food shoping',
+       day : 'feb 5th at 2:30pm',
+       reminder : false,
+   }
+])
+//add task via submiting form
+const addTask=(task)=>{
+ const id =Math.random();
+ const newTask ={id,...task}
+ setTasks([...tasks,newTask])
+
+}
+// delete task using array filter method
+const deleteTask=(id)=>{
+  setTasks(tasks.filter((task)=> task.id!==id))
+}
+//Toggle reminder
+const toggleReminder=(id)=>{
+    setTasks(tasks.map((task)=>task.id===id ? {...task,reminder:!task.reminder} : task))
+} 
+ return (
+    <div className="container">
+        <Header  onAdd={()=>{
+          setShowAddTask(!showAddTask)
+        }} showAdd={showAddTask}/>
+        {showAddTask && <AddTask onAdd={addTask}/>}
+        {tasks.length>0 ?
+        ( <Tasks tasks = {tasks} onDelete ={deleteTask}
+          onToggle={toggleReminder} />
+        ):(
+          <h3>no task to perform </h3>
+          
+        )}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+
+import Button from './Button'
+
+const Header = ({title,onAdd,showAdd}) => {
+
+    return (
+<header className ='header'>
+    <h1 >
+    {title}   
+    </h1>
+    <Button color ={showAdd ? 'red' : 'green'} text = {showAdd ? 'close':'add'} onClick = {onAdd}/>
+         
+        </header>
+    )
+}
+Header.defaultProps={
+    title: 'default task tracker',
+}
+export default Header
+//PropTypes here
+// Header.prototype={
+//     title: PropTypes.string.isRequired,
+// }
+
+//CSS here
+// const headingStyle ={
+//     color : 'red',backgroundColor:'black',
+// }
+
